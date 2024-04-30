e91980c7bfc5e99f22223c78c5486a70a5b7a2d4
Kenlark <kenzokerachi@hotmail.fr>
2024-04-30 10:01:12 +0200

    accueil

--- burger.js
+++ burger.js
@@ -5,7 +5,7 @@ function toggleMenu() {
     const navbar = document.querySelector('.navbar'); // Sélectionnez la barre de menu
 
     if (menu.classList.contains('active')) {
-        navbar.style.height = '20%'; // Augmentez la hauteur de la barre de menu à 20%
+        navbar.style.height = '17%'; // Augmentez la hauteur de la barre de menu à 20%
     } else {
         navbar.style.height = '5%'; // Rétablissez la hauteur de la barre de menu à 5%
     }
